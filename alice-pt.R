47df6060a8fbfde42b22e23efda202abfcea2896
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-11-06 11:12:16 +0100

    Add model computation

--- alice-pt.R
+++ alice-pt.R
@@ -41,3 +41,44 @@ levels(df$Definiteness_Recipient)
 levels(df$Affectedness_Recipient)
 levels(df$Topic)
 levels(df$Theme_Form)
+
+# Define the dataset
+ds <- dataset(df=df,
+              response_variable_column="Construction",
+              to_binary_columns=c("Lemma", "Genre"),
+              other_columns=c("Register", "Focus"))
+
+# Define Elastic Net object
+net <- elastic_net(ds=ds,
+                   nfolds=20,
+                   type.measure="class")
+
+# Compute Elastic Net
+output <- net$do_elastic_net_regression_auto_alpha(k=20)
+
+# Find the model with the lowest loss
+lowest_loss_row <- output$results[which.min(output$results$loss),]
+lowest_loss_row
+
+coefficients_with_labels <- net$attach_coefficients(
+  output$fits[[lowest_loss_row[["X_id"]]]])
+
+# Export
+write.csv(coefficients_with_labels, "output/alice-pt.csv",
+          row.names=FALSE)
+
+#
+# Export model information
+#
+
+model_meta <- meta.file()
+
+for (attribute in colnames(lowest_loss_row)) {
+  if (attribute == "X_id") {
+    next
+  }
+  
+  model_meta$add_model_information(attribute, lowest_loss_row[[attribute]])
+}
+
+write.csv(model_meta$as.data.frame(), "output/model_meta.csv", row.names=FALSE)
